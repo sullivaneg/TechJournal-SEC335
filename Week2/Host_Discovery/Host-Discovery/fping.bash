ea5f295c7b79f9c3a99572086934660e9bb2702a
sullivaneg <emma.sullivan@mymail.champlain.edu>
2026-04-08 17:05:48 -0400

    removing repeat directories

--- Week2/Host_Discovery/Host-Discovery/fping.bash
+++
@@ -1,1 +0,0 @@
-fping -a -g 10.0.5.2 10.0.5.50 >> fpingsweep.txt
--- Week3/Powershell-and-DNS/dns-resolver.ps1
+++
@@ -1,9 +0,0 @@
-param ($network_prefix, $dns_server)
-Write-Host "ip, resolved name"
-for ($i=1; $i -lt 254; $i++) {
-    $ip = "$network_prefix.$i"
-    $hostname = Resolve-DnsName -DnsOnly $ip -Server $dns_server -ErrorAction Ignore
-    if ($hostname.NameHost) {
-        Write-Host "$ip $($hostname.NameHost)"
-    }
-}
